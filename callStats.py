3f6340d80c1808f8f7df2d4ef87181103881d260
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2021-06-21 14:02:40 -0400

    Add user tracking functionality

--- callStats.py
+++ callStats.py
@@ -1,11 +1,10 @@
+# Imports
 import discord
 from discord.embeds import Embed
 from discord.ext import commands
 from discord.ext.commands import Bot
-from textwrap import dedent
-
-# Just so our code doesn't look bad
 from discord.ext.commands.errors import ConversionError, MissingRequiredArgument
+import time
 
 # Reads the bot token from file
 with open("token.txt") as fp:
@@ -33,38 +32,91 @@ class ChannelConverter(commands.Converter):
 
 class RecordingCommands(commands.Cog):
     # Cog containing all commands used during recording for the bot after start
-    # Intializes with the voiceChannel it started recording on
     def __init__(self, bot) -> None:
         super().__init__()
-        # Initialized with a reference to the bot and the voice channel the recording is on
+        # Initialized with a reference to the bot and the voice channel set to None
         self.bot = bot
         self.voiceChannel = None
+        self.users = []
+        self.ctx = None
 
     @commands.command(name="stop", brief="Stops any active recordings")
     async def stop_command(self, ctx):
-        if self.voiceChannel == None:
+        if self.voiceChannel == None:  # Check for an active recording
             await ctx.send("There is no active recording")
         else:
-            await ctx.send("Stopped recording on channel: %s." % str(self.voiceChannel))
-            # Set the current voice channel to None
+            await ctx.send(f"Stopped recording on channel: {self.voiceChannel}")
+            # Reset all of the variables
             self.voiceChannel = None
+            self.textChannel = None
+            self.users = []
 
     @commands.command(name="check", brief="Reports the status of an active recording")
-    async def system_check_command(self, ctx):
-        await ctx.send("This does nothing yet")
+    async def check_command(self, ctx):
+        if self.voiceChannel == None:  # Check for an active recording
+            await ctx.send("There is no active recording")
+        else:
+            await ctx.send("This does nothing yet")
 
 
-# Starts recording the channel stats
+class UserStats:  # Class used by a list in the recording cog that details user stats
+    def __init__(self, user: discord.Member) -> None:
+        self.user = user  # user object to be added of discord abc class
+        self.joinTimes = []  # Details any time the user joins the voice channel
+        self.leaveTimes = []  # Details whenever this user has left the voice channel
+
+    def __eq__(self, o: object) -> bool:
+        return self.user.id == o.user.id
+
+
 @bot.command(name="start", brief="Starts recording on specified channel")
 async def start_command(ctx, voiceChannel: ChannelConverter()):
-    cogCheck = bot.get_cog("RecordingCommands")
+    # Starts recording the channel stats
+    activeCog = bot.get_cog("RecordingCommands")
     # Check if there already is a recording
-    if cogCheck.voiceChannel is not None:
+    if activeCog.voiceChannel is not None:
         await ctx.send("There is already a recording in process")
     else:
         # If there is no recording, add the RecordingCog and send a message
-        cogCheck.voiceChannel = voiceChannel
+        activeCog.voiceChannel = voiceChannel
+        activeCog.ctx = ctx
+        # Add any currently connected users to user list
+        for memberID in voiceChannel.voice_states:
+            activeCog.users.append(UserStats(ctx.guild.get_member(memberID)))
+
         await ctx.send(f"Started recording on channel: {voiceChannel}!")
+
+
+@bot.listen("on_voice_state_update")  # Listener for voice activity
+# Member is type discord.Member and before and after are type discord.VoiceState
+async def call_activity(member, before, after):
+    # First we check if its on the actively recorded channel
+    activeCog = bot.get_cog("RecordingCommands")  # Get the recording cog
+
+    # If there is no active recording, we can stop already
+    if activeCog.voiceChannel == None:
+        return
+
+    # Define a new UserStats object to be used in the conditionals
+    newUser = UserStats(member)
+
+    # Check if it's the recorded channel when joining
+    if after.channel == activeCog.voiceChannel and before.channel != activeCog.voiceChannel:
+
+        # Check if the user is already in the list of users
+        if newUser in activeCog.users:
+            await activeCog.ctx.send("A prior Member joins")
+        else:  # In case it is a new user
+            await activeCog.ctx.send("A new Member joins")
+            activeCog.users.append(newUser)
+    # Check if it's the recorded channel when leaving
+    elif before.channel == activeCog.voiceChannel and after.channel != activeCog.voiceChannel:
+        if newUser in activeCog.users:  # This should always be the case
+            await activeCog.ctx.send("A prior Member leaves")
+        else:  # If somehow a user snuck in without being notices this will occur
+            await activeCog.ctx.send("Something went wrong")
+
+    # Otherwise this is not the right channel and we ignore
 
 
 @start_command.error  # Error message for failed start command
@@ -83,18 +135,18 @@ async def start_error(ctx, error):
         raise error
 
 
-@bot.listen("on_command_error")
-async def warn_on_command_cooldown(ctx, error):
+@bot.listen("on_command_error")  # Error message if user tries bad command
+async def warn_on_command_error(ctx, error):
     # Unwrapping the error cause because of how discord.py raises some of them
     error = error.__cause__ or error
 
     if isinstance(error, commands.CommandNotFound):
         # Direct the user to use the help command if they try a bad command
-        await ctx.send("It seems that command doesnt exist, try cs-help")
+        await ctx.send("It seems that command doesnt exist. Try cs-help for a list of commands")
 
 # Add the recording commands cog to the bot
 bot.add_cog(RecordingCommands(bot))
 
-# Never forget to leave this on the bottom of your code
+# Start the bot
 # Anything after this line will only be executed once the bot logs out
 bot.run(TOKEN)
