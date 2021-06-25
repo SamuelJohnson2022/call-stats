f0f6cae44245b73a5e3bda4d73e638a4f47833bb
SamuelJohnson2022 <43099841+SamuelJohnson2022@users.noreply.github.com>
2021-06-25 13:46:06 -0400

    Put start into RecordingCommands cog, add comments

--- callStats.py
+++ callStats.py
@@ -60,12 +60,34 @@ class RecordingCommands(commands.Cog):
         self.users = {}  # Dictionary to store user data with key being their discord ID
         self.ctx = None
 
+    @commands.command(name="start", brief="Starts recording on specified channel")
+    async def start_command(self, ctx, voiceChannel: ChannelConverter()):
+        # Starts recording the channel stats
+        # Check if there already is a recording
+        if self.voiceChannel is not None:
+            await ctx.send("There is already a recording in process")
+        else:
+            # If there is no recording, add the RecordingCog and send a message
+            self.voiceChannel = voiceChannel
+            self.ctx = ctx
+            # Add any currently connected users to user list
+            for memberID in voiceChannel.voice_states:
+                # This get_member function only works with the members intent activated
+                self.users[memberID] = UserStats(
+                    ctx.guild.get_member(memberID))
+
+                # Append the current time as the user's "join time"
+                self.users[memberID].joinTimes.append(time.time())
+
+            # Send a message so the user knows a recording has started
+            await ctx.send(f"Started recording on: **{voiceChannel}**")
+
     @commands.command(name="stop", brief="Stops any active recordings")
     async def stop_command(self, ctx):
         if self.voiceChannel == None:  # Check for an active recording
             await ctx.send("There is no active recording")
         else:
-            await ctx.send(f"Stopped recording on channel: {self.voiceChannel}")
+            await ctx.send(f"Stopped recording on channel: **{self.voiceChannel}**")
 
             # Make each of the users current in call "leave"
             for memberID in self.voiceChannel.voice_states:
@@ -130,13 +152,29 @@ class RecordingCommands(commands.Cog):
             for memberID in self.voiceChannel.voice_states:
                 self.users[memberID].leaveTimes.pop()
 
+    @ start_command.error  # Error message for failed start command
+    async def start_error(self, ctx, error):
+        # Unwrapping the error cause because of how discord.py raises some of them
+        error = error.__cause__ or error
+        if isinstance(error, ValueError):
+            # Tell the user that the channel they specified was non-existant
+            await ctx.send("That voice channel does not exist. (Remember to be case sensitive)")
+        elif isinstance(error, MissingRequiredArgument):
+            # Tell the user they need a channel name argument
+            await ctx.send("Missing a name for the target voice channel")
+        else:
+            # Final case where the error is not covered by this handler
+            await ctx.send("Something went wrong.")
+            raise error
+
 
 class UserStats:  # Class used by a list in the recording cog that details user stats
     def __init__(self, user: discord.Member) -> None:
-        self.user = user  # user object to be added of discord abc class
+        self.user = user  # user object to be added of discord Member class
         self.joinTimes = []  # Details any time the user joins the voice channel
         self.leaveTimes = []  # Details whenever this user has left the voice channel
 
+    # Used when iterating over the dictionary of UserStats objects
     def __eq__(self, o: object) -> bool:
         return self.user.id == o.user.id
 
@@ -150,8 +188,6 @@ def createChart(users: dict):
     colorDict = dict()
     # Iterate through all of the users
     for key in users:
-
-        # Add their color to the color list
 
         # If the user doesnt have an avatar, find the color of their default avatar
         if users[key].user.avatar == None:
@@ -172,7 +208,7 @@ def createChart(users: dict):
             # Get the url of the user avatar
             urlDict[users[key].user.name] = str(users[key].user.avatar_url)
             response = requests.get(users[key].user.avatar_url)
-            # Find the dominant color of their avatar  using clusters
+            # Find the dominant color of their avatar  using clusters - credit Peter Hansen on Stack Overflow
             with Image.open(BytesIO(response.content)) as im:
                 im = im.resize((150, 150))      # optional, to reduce time
                 ar = np.asarray(im)
@@ -208,7 +244,7 @@ def createChart(users: dict):
 
             dictList.append(statsDict)
 
-    # Create hte data frame used by plotly
+    # Create the pandas data frame used by plotly
     df = pd.DataFrame([data for data in dictList])
 
     # Create the actual gantt chart
@@ -248,28 +284,6 @@ def createChart(users: dict):
     return
 
 
-@bot.command(name="start", brief="Starts recording on specified channel")
-async def start_command(ctx, voiceChannel: ChannelConverter()):
-    # Starts recording the channel stats
-    activeCog = bot.get_cog("RecordingCommands")
-    # Check if there already is a recording
-    if activeCog.voiceChannel is not None:
-        await ctx.send("There is already a recording in process")
-    else:
-        # If there is no recording, add the RecordingCog and send a message
-        activeCog.voiceChannel = voiceChannel
-        activeCog.ctx = ctx
-        # Add any currently connected users to user list
-        for memberID in voiceChannel.voice_states:
-            # This get_member function doesnt work if user is in the voice channel before starting the bot
-            activeCog.users[memberID] = UserStats(
-                ctx.guild.get_member(memberID))
-
-            activeCog.users[memberID].joinTimes.append(time.time())
-
-        await ctx.send(f"Started recording on channel: {voiceChannel}!")
-
-
 @ bot.listen("on_voice_state_update")  # Listener for voice activity
 # Member is type discord.Member and before and after are type discord.VoiceState
 async def call_activity(member, before, after):
@@ -293,15 +307,11 @@ async def call_activity(member, before, after):
                 activeCog.users[member.id].joinTimes.pop()
                 activeCog.users[member.id].leaveTimes.pop()
 
-            # Temp debug output
-            await activeCog.ctx.send(f"{activeCog.users[member.id].user.name} joins: {activeCog.users[member.id].joinTimes[-1]}")
         else:  # In case it is a new user
             # Add them to the users dictionary
             activeCog.users[member.id] = UserStats(member)
             # Append their time to the joinTimes list for that user
             activeCog.users[member.id].joinTimes.append(time.time())
-            # Temp debug output
-            await activeCog.ctx.send(f"{activeCog.users[member.id].user.name} joins: {activeCog.users[member.id].joinTimes[-1]}")
 
     # Check if it's the recorded channel when leaving
     elif before.channel == activeCog.voiceChannel and after.channel != activeCog.voiceChannel:
@@ -311,28 +321,10 @@ async def call_activity(member, before, after):
                 activeCog.users[member.id].user = member
             # Append their time to the leaveTimes list for that user
             activeCog.users[member.id].leaveTimes.append(time.time())
-            # Temp debug output
-            await activeCog.ctx.send(f"{activeCog.users[member.id].user.name} leaves: {time.ctime(activeCog.users[member.id].leaveTimes[-1])}")
         else:  # If somehow a user snuck in without being notices this will occur
             await activeCog.ctx.send("Something went wrong")
 
     # Otherwise this is not the right channel and we ignore
-
-
-@ start_command.error  # Error message for failed start command
-async def start_error(ctx, error):
-    # Unwrapping the error cause because of how discord.py raises some of them
-    error = error.__cause__ or error
-    if isinstance(error, ValueError):
-        # Tell the user that the channel they specified was non-existant
-        await ctx.send("That channel does not exist. (Remember to be case sensitive)")
-    elif isinstance(error, MissingRequiredArgument):
-        # Tell the user they need a channel name argument
-        await ctx.send("Missing a name for the target channel")
-    else:
-        # Final case where the error is not covered by this handler
-        await ctx.send("Something went wrong.")
-        raise error
 
 
 @ bot.listen("on_command_error")  # Error message if user tries bad command
